9c8377cb5730cde931c7191ec37794aebef7e78c
Rudnasoukadanicia <rsouka@byupathway.edu>
2026-05-07 15:47:58 +0100

    cse340_w1

---
+++ server.js
@@ -0,0 +1,58 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import express from 'express';
+
+// Define the the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+// define the port number the server will listen on, using an environment variable or defaulting to 3000
+
+const PORT = process.env.PORT || 3000;
+// get the current file path and directory
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the port number the server will listen on
+const app = express();
+
+/**
+ * Configure Express middleware
+    */
+
+// Serve static files from the public directory
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
+
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+app.get('/organizations', async (req, res) => {
+    const title = 'Organizations';
+    res.render('organizations',{title})
+});
+app.get('/projects', async (req, res) => {
+    const title = 'Projects';
+    res.render('projects',{title})
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!(using nodemon)');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+});
+
+//Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+//Tell Express where to find the EJS templates
+app.set('views', path.join(__dirname, 'src/views'));
