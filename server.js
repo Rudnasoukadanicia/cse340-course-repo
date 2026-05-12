6576e85b24294114b820ec007d97be4f8098a2e2
Rudnasoukadanicia <rsouka@byupathway.edu>
2026-05-12 15:34:41 +0100

    w2_

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -33,8 +35,10 @@ app.get('/', async (req, res) => {
     res.render('home', { title });
 });
 app.get('/organizations', async (req, res) => {
-    const title = 'Organizations';
-    res.render('organizations', { title })
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
 });
 app.get('/projects', async (req, res) => {
     const title = 'Projects';
@@ -46,17 +50,22 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title })
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!(using nodemon)');
+
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
-});
 
 //Set EJS as the templating engine
 app.set('view engine', 'ejs');
 
 //Tell Express where to find the EJS templates
 app.set('views', path.join(__dirname, 'src/views'));
+
+
